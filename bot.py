ce5b30b2dde1b3149e62730b2afd291b2efa2599
bway9841-code <bway9841@gmail.com>
2026-09-05 23:01:17 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,21 @@
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot is running!"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
+
+keep_alive()
+
 import os
 import discord
 from discord.ext import commands
@@ -106,4 +124,4 @@ async def setupmenu(ctx):
     await ctx.send(embed=embed, view=view)
 
 bot.run(os.getenv("TOKEN"))
-      
+
